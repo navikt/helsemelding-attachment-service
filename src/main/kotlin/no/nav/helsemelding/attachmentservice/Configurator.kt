6c018989ffa5bcef3a69ab12bba12095a3e7bd33
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-04-28 12:15:34 +0200

    Opprettet grunnlaget for web tjeneste

---
+++ src/main/kotlin/no/nav/helsemelding/attachmentservice/Configurator.kt
@@ -0,0 +1,18 @@
+package no.nav.helsemelding.attachmentservice
+
+import arrow.core.memoize
+import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.ExperimentalHoplite
+import com.sksamuel.hoplite.addResourceSource
+import no.nav.helsemelding.attachmentservice.config.Config
+
+@OptIn(ExperimentalHoplite::class)
+val config: () -> Config = {
+    ConfigLoader.builder()
+        .addResourceSource("/application-personal.conf", optional = true)
+        .addResourceSource("/application.conf")
+        .withExplicitSealedTypes()
+        .build()
+        .loadConfigOrThrow<Config>()
+}
+    .memoize()
