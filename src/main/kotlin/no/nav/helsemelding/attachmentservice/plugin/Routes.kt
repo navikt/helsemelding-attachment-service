0581de299c87fa9944b59b34c331a752ca29ef4b
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-05-04 16:47:12 +0200

    Lagt til test endepunkter

--- src/main/kotlin/no/nav/helsemelding/attachmentservice/plugin/Routes.kt
+++ src/main/kotlin/no/nav/helsemelding/attachmentservice/plugin/Routes.kt
@@ -1,21 +1,28 @@
 package no.nav.helsemelding.attachmentservice.plugin
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import no.nav.helsemelding.attachmentservice.model.Attachment
+import no.nav.helsemelding.attachmentservice.repository.AttachmentRepository
 
 fun Application.configureRoutes(
-    registry: PrometheusMeterRegistry
+    registry: PrometheusMeterRegistry,
+    attachmentRepository: AttachmentRepository
 ) {
-    routing { internalRoutes(registry) }
+    routing { internalRoutes(registry, attachmentRepository) }
 }
 
-fun Route.internalRoutes(registry: PrometheusMeterRegistry) {
+fun Route.internalRoutes(registry: PrometheusMeterRegistry, attachmentRepository: AttachmentRepository) {
     get("/prometheus") {
         call.respond(registry.scrape())
     }
@@ -25,6 +32,49 @@ fun Route.internalRoutes(registry: PrometheusMeterRegistry) {
         }
         get("/health/readiness") {
             call.respondText("I'm ready! :)")
+        }
+    }
+
+    // TODO: For testing purpose only. Remove before merge
+    route("/attachments") {
+        post("/test") {
+            try {
+                val messageId = "8ccc4474-47c7-11f1-83a5-325096b39f47"
+                val testAttachments = listOf(
+                    Attachment(
+                        fileName = "attachment.txt",
+                        contentType = "text/plain",
+                        content = "Arbitrary text here".toByteArray()
+                    ),
+                    Attachment(
+                        fileName = "attachment2.txt",
+                        contentType = "text/plain",
+                        content = "More arbitrary text here".toByteArray()
+                    )
+                )
+
+                attachmentRepository.save(messageId, testAttachments)
+            } catch (e: Exception) {
+                call.respondText("Error saving attachment: ${e.message}")
+                return@post
+            }
+
+            call.respondText("OK")
+        }
+
+        get("/test") {
+            try {
+                val messageId = "8ccc4474-47c7-11f1-83a5-325096b39f47"
+                val content = attachmentRepository.read(messageId)
+
+                if (content.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                } else {
+                    call.respondText(Json.encodeToString(content))
+                }
+            } catch (e: Exception) {
+                call.respondText("Error reading attachment: ${e.message}")
+            }
         }
     }
 }
