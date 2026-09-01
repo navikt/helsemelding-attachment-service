0429af569c76af42974b65afd34bb36ebab88831
OleksandrChmyrNAV <oleksandr.chmyr@nav.no>
2026-09-01 11:45:29 +0200

    Fix: Vedlegget finnes allerede (#29)
    * Utført et sjekk i tilfelle vedlegget finnes allerede
    
    * Fjernet unødvendige kall til GCS
    
    * Fikset semantikken i tester

--- src/test/kotlin/no/nav/helsemelding/attachmentservice/repository/GcsAttachmentRepositorySpec.kt
+++ src/test/kotlin/no/nav/helsemelding/attachmentservice/repository/GcsAttachmentRepositorySpec.kt
@@ -3,6 +3,7 @@ package no.nav.helsemelding.attachmentservice.repository
 import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.Storage
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -24,18 +25,37 @@ class GcsAttachmentRepositorySpec : StringSpec({
         bucketName = bucketName
     )
 
-    "save should store attachments in GCS bucket" {
+    "save should store attachments in GCS bucket when they do not exist" {
         val blob = mockk<Blob>()
-
         val content = Json.encodeToString(testAttachments).toByteArray()
 
-        every {
-            storage.create(any<BlobInfo>(), content)
-        } returns blob
+        every { storage.create(any<BlobInfo>(), content) } returns blob
 
         val result = repository.save(messageId, testAttachments)
 
         result shouldBe content.size
+    }
+
+    "save should skip storing attachment when it already exist" {
+        val existingBlob = mockk<Blob>()
+        val existingContent = Json.encodeToString(testAttachments).toByteArray()
+
+        every { storage.create(any<BlobInfo>(), any<ByteArray>()) } throws RuntimeException("retention policy")
+        every { storage.get(bucketName, messageId.toString()) } returns existingBlob
+        every { existingBlob.getContent() } returns existingContent
+
+        val result = repository.save(messageId, testAttachments)
+
+        result shouldBe existingContent.size
+    }
+
+    "save should rethrow exception when create fails and attachments do not exist" {
+        every { storage.create(any<BlobInfo>(), any<ByteArray>()) } throws RuntimeException("network error")
+        every { storage.get(bucketName, messageId.toString()) } returns null
+
+        shouldThrow<RuntimeException> {
+            repository.save(messageId, testAttachments)
+        }
     }
 
     "read should read attachments from GCS bucket" {
